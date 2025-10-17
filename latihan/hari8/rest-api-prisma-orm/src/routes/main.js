3f46c0105dcc126b8659cc124dc12785e59750b7
Sudo-Lucius <wolfaditya7474@gmail.com>
2025-10-17 12:48:11 +0800

    Menambah File

--- latihan/hari8/rest-api-prisma-orm/src/routes/main.js
+++ latihan/hari8/rest-api-prisma-orm/src/routes/main.js
@@ -5,6 +5,10 @@ const { readMovieById } = require('../controllers/movieController')
 const { updateMovie } = require('../controllers/movieController')
 const { deleteMovie } = require('../controllers/movieController')
 const { basicAuth } = require('../middleware/basicAuth')
+const { createCategory } = require('../controllers/categoryController')
+const { readCategory } = require('../controllers/categoryController')
+const { updateCategory } = require('../controllers/categoryController')
+const { deleteCategory } = require('../controllers/categoryController')
 const router = express.Router()
 
 
@@ -13,5 +17,11 @@ router.get('/movies',  readMovie)
 router.get('/movies/:id',  readMovieById)
 router.put('/movies/:id', basicAuth ,updateMovie)
 router.delete('/movies/:id', basicAuth ,deleteMovie)
+
+router.post('/category', basicAuth ,createCategory)
+router.get('/category',  readCategory)
+router.get('/category/:id',  readMovieById)
+router.put('/category/:id', basicAuth ,updateCategory)
+router.delete('/category/:id', basicAuth ,deleteCategory)
 
 module.exports = router
